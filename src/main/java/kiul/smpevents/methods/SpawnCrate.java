955a25fa48b29e37e52b0bb16f91b2e6f4d457e1
KiulWasTaken <pvpkylecr07@gmail.com>
2023-09-14 15:18:20 +0930

    misc additions ; - added quarter-timely notifications as to where the crate is going to drop - added the time-up detection method - decided that restart-continuity between crates is not required
    Took 55 minutes

--- src/main/java/kiul/smpevents/methods/SpawnCrate.java
+++ src/main/java/kiul/smpevents/methods/SpawnCrate.java
@@ -1,16 +1,30 @@
 package kiul.smpevents.methods;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
+import kiul.smpevents.SMPEvent;
+import kiul.smpevents.config.TimeData;
+import org.bukkit.*;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Date;
 import java.util.Random;
 
 public class SpawnCrate {
 
-    public void spawnCrate (String type, int minutesUntilSpawn, int unlockMinutes, World world) {
+
+
+    public void createNewCrate (String type, int minutesUntilSpawn, World world) {
+        if (TimeData.get().get("crate.number") == null) {
+            TimeData.get().set("crate.number", 0);
+        }
+        int crateNumber = TimeData.get().getInt("crate.number");
+        long crateSpawnTime = (System.currentTimeMillis() + minutesUntilSpawn*60*1000);
+        long remainingTime = (crateSpawnTime - System.currentTimeMillis());
         Random random = new Random();
+
         int x = random.nextInt(0,7900);
         int z = random.nextInt(0,7900);
         int y = world.getHighestBlockYAt(x,z);
@@ -19,5 +33,28 @@ public class SpawnCrate {
         Bukkit.broadcastMessage(type + " crate will drop at " + x + " " + y + " " + z + " in " + minutesUntilSpawn + " minutes.");
         Bukkit.broadcastMessage("");
         System.currentTimeMillis();
+        new BukkitRunnable() {
+            int tick = 1;
+            @Override
+            public void run() {
+                if (System.currentTimeMillis() > crateSpawnTime) {
+                    spawnCrate(type,20,world,crateSpawnLocation);
+                }
+                    tick++;
+                if (tick >= (((remainingTime/4)/1000)/60)) {
+                    Bukkit.broadcastMessage("");
+                    Bukkit.broadcastMessage(type + " drop will arrive at " + x + " " + y + " " + z + " in " + (((crateSpawnTime - System.currentTimeMillis())/1000)/60) + " minutes.");
+                    Bukkit.broadcastMessage("");
+                    tick = 1;
+                }
+
+
+
+            }
+        }.runTaskTimer(SMPEvent.getPlugin(SMPEvent.class), 0L, 1200L);
+    }
+
+    public void spawnCrate (String type, int unlockMinutes, World world,Location spawnlocation) {
+
     }
 }
